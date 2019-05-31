fc3f4afd82fa95cda9c0e87ae30a8116ffcfca3d
cyi <cyi@creatcomm.com>
2019-05-31 11:54:53 +0800

    support platform use 1.fix update key error 2.add checkout work path change to /dev/shm 3.support exec everywhere 4.add repo suppor script gitcheckout 5.fix checkout branch name to set 6.support change url when is diff with .git dir

--- SparseCheckout.sh
+++ SparseCheckout.sh
@@ -5,8 +5,12 @@ RESPOSITORY_PATH=${2}
 REMOTE_URL=${3}
 COMMIT_BRANCH=${4}
 CHECKOUT_LIST=${@:5:$#-4}
+WORK_PATH="/dev/shm/.checkout_$(id -u)"
 
-cd "${RESPOSITORY_PATH}"
+if [ ! -d "$WORK_PATH" ]; then
+    mkdir -p $WORK_PATH
+fi
+cd "${WORK_PATH}"
 
 git config --global core.sparseCheckout true
 
@@ -18,17 +22,26 @@ if [ ! -d ".git" ]; then
     done
     git pull origin "${COMMIT_BRANCH}" --depth=1
 else
-	for i in ${CHECKOUT_LIST}; do
-		if [[ `grep -c ${i} ".git/info/sparse-checkout"` -eq '0' ]]; then
-			echo ${i} >> ".git/info/sparse-checkout"
-		else
-			echo "Found! ${i}"
-		fi
-	done
+    gitpath=`git remote -v | grep fetch | awk -F" " '{print $2}'`
+    if [ "$gitpath" != "$REMOTE_URL" ]; then
+        git remote set-url origin "${REMOTE_URL}"
+        echo "" > .git/info/sparse-checkout
+    fi
+    for i in ${CHECKOUT_LIST}; do
+        if [[ `grep -c ${i} ".git/info/sparse-checkout"` -eq '0' ]]; then
+            echo ${i} >> ".git/info/sparse-checkout"
+        else
+            echo "Found! ${i}"
+        fi
+    done
 fi
 
-if [[ ${UPDATE} = "ture" ]]; then
+if [[ ${UPDATE} = "true" ]]; then
     git pull origin "${COMMIT_BRANCH}" --depth=1
 fi
 
-git checkout -qf master
+git checkout -qf ${COMMIT_BRANCH}
+for i in ${CHECKOUT_LIST}; do
+    mv ${i} $RESPOSITORY_PATH/
+done
+
